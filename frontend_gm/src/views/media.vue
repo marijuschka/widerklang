3ffbc96cbf836bf1926f4ed3c87e1809c9849b19
Jonas <jonasmanten@yahoo.de>
2019-07-22 14:14:34 +0200

    Medienverwaltung
    Datei Medienverwaltung angelegt.
    Tests mache ich lokal.

---
+++ frontend_gm/src/views/media.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login">
+    <Header />
+    <br />
+    <img class="logo" alt="Vue logo Responsive image" src="../assets/WiderklangLogo.png" />
+    <br />
+    <br />
+    <h1>Medienverwaltung!</h1>
+
+   
+ 
+
+    <br />
+    <br />
+    <br />
+
+
+
+
+    
+  </div>
+</template>
+
+<script>
+
+
+
+export default 
+</script>
+
+
+<style scoped>
+.logo {
+  width: 300px;
+}
+</style>
