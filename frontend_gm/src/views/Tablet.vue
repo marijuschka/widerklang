c7517b99921c575f810580882b7b416ce07b1764
Kutsche25 <Kutsche25@web.de>
2019-07-24 11:48:47 +0200

    Tablet Seite jetzt mit 4 Themen-Boxen

--- frontend_gm/src/views/Tablet.vue
+++ frontend_gm/src/views/Tablet.vue
@@ -2,8 +2,43 @@
   <div id="app">
     <div class="tablet">
       <Header />
-      <br>
-      HIER KOMMT DIE TABLET ANWENDUNG HIN
+      <br />
+
+      <div class="container">
+        <div class="row">      
+          <div class="col 6 theme-frame">
+            <a href="/" class="theme-text">
+              <div class="col 6 theme-boxes red-theme">
+                <h1>Familie & Freundschaft</h1>
+              </div>
+            </a>
+          </div>
+          <div class="col 6 theme-frame">
+            <a href="/" class="theme-text align-middle">
+              <div class="col 6 theme-boxes blue-theme">
+                <h1>Heimat & Regionales</h1>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="row">      
+          <div class="col 6 theme-frame">
+            <a href="/" class="theme-text">
+              <div class="col 6 theme-boxes green-theme">
+                <h1>Natur & Tierwelt</h1>
+              </div>
+            </a>
+          </div>
+          <div class="col 6 theme-frame">
+            <a href="/" class="theme-text">
+              <div class="col 6 theme-boxes orange-theme">
+                <h1>Freizeit & Vergnügen</h1>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -13,15 +48,42 @@ import Header from "../components/Header.vue";
 
 export default {
   name: "tablet",
-  components: { 
+  components: {
     Header
   }
 };
 </script>
 
 <style>
-img{
-  width:400px
+img {
+  width: 400px;
 }
-
+.theme-boxes {
+  height: 30vh;
+  padding: 0;
+  border-radius: 50px;
+  box-shadow: 0px 5px 5px #303030;
+}
+.theme-frame {
+  margin-top: 20px;
+}
+.theme-text {
+  color: white;
+}
+.theme-text:hover{
+  color: pink;
+  Text-Decoration: None !important;
+}
+.red-theme {
+  background-color: #DB1644;
+}
+.blue-theme {
+  background-color: #50A5EB;
+}
+.green-theme {
+  background-color: #32A62E;
+}
+.orange-theme {
+  background-color: #F2BC18;
+}
 </style>
