6e0a491c6ee9ff81652e39a0f8f3cc68301f382a
Kutsche25 <Kutsche25@web.de>
2019-07-24 09:38:35 +0200

    Jonas, deine Media.vue Datei war klein geschrieben und deshalb gabs Probleme

--- frontend_gm/src/views/media.vue
+++ frontend_gm/src/views/media.vue
@@ -2,14 +2,13 @@
   <div id="app">
     <div class="media">
       <Header />
-      <br>
-      MEDIENVERWALTUNG
+      <br />MEDIENVERWALTUNG
       <b-col cols="2" md="1">
-              <b-card-img src="https://picsum.photos/id/870/200/300?grayscale&blur=2"></b-card-img>
-              <b-card-img src="https://picsum.photos/id/870/200/300?grayscale&blur=2"></b-card-img>
-              <b-card-img src="https://picsum.photos/id/870/200/300?grayscale&blur=2"></b-card-img>
-              </b-col>
-                     </div>
+        <b-card-img src="https://picsum.photos/id/870/200/300?grayscale&blur=2"></b-card-img>
+        <b-card-img src="https://picsum.photos/id/870/200/300?grayscale&blur=2"></b-card-img>
+        <b-card-img src="https://picsum.photos/id/870/200/300?grayscale&blur=2"></b-card-img>
+      </b-col>
+    </div>
   </div>
 </template>
 
@@ -18,15 +17,14 @@ import Header from "../components/Header.vue";
 
 export default {
   name: "media",
-  components: { 
+  components: {
     Header
   }
 };
 </script>
 
 <style>
-img{
-   width: 70px;
+img {
+  width: 70px;
 }
-
 </style>
