6869dc40f507695b187b4f366c03639a6263f2ca
tillweyer <tillweyer@hotmail.de>
2019-08-14 16:12:41 +0200

    Front - Schnellzugriff Design

--- frontend_gm/src/views/Schnellzugriff.vue
+++ frontend_gm/src/views/Schnellzugriff.vue
@@ -5,11 +5,15 @@
     <b-row class="justify-content-center">
       <!-- Linke Seite -->
       <b-col cols="5" id="links">
-        <h2 id="konten">KONTEN</h2>
+        <router-link to="/user" id="user"><h2 id="konten">NUTZER</h2></router-link>
+        
         <hr />
+
+        <h3>Schnellzugriff</h3>
         <b-row class="justify-content-center">
-          <b-col cols="6">
+          <b-col cols="4">
             <b-img
+              class="rounded"
               rounded="circle"
               thumbnail
               src="https://images.unsplash.com/photo-1472066719480-ecc7314ed065?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
@@ -17,8 +21,9 @@
               alt="Fluid image"
             ></b-img>
           </b-col>
-          <b-col cols="6">
+          <b-col cols="4">
             <b-img
+            class="rounded"
               rounded="circle"
               thumbnail
               src="https://images.unsplash.com/photo-1472066719480-ecc7314ed065?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
@@ -26,8 +31,12 @@
               alt="Fluid image"
             ></b-img>
           </b-col>
-          <b-col cols="6">
+        </b-row>
+        <br>
+        <b-row class="justify-content-center">
+          <b-col cols="4">
             <b-img
+            class="rounded"
               rounded="circle"
               thumbnail
               src="https://images.unsplash.com/photo-1472066719480-ecc7314ed065?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
@@ -35,8 +44,9 @@
               alt="Fluid image"
             ></b-img>
           </b-col>
-          <b-col cols="6">
+          <b-col cols="4">
             <b-img
+            class="rounded"
               rounded="circle"
               thumbnail
               src="https://images.unsplash.com/photo-1472066719480-ecc7314ed065?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
@@ -51,69 +61,67 @@
 
       <!-- Rechte Seite -->
       <b-col cols="5">
-        <h2>MEDIEN</h2>
+        <router-link to="/mediaManagement" id="mediaManagement"><h2 id="konten">MEDIEN</h2></router-link>
         <hr />
+        <h3>Schnellzugriff</h3>
         <div class="container">
-          <div class="row" >
-            <div class="col 6 theme-frame">
+          <div class="row" align="center">
+            <!-- Zur Zentrieerung -->
+            <div class="col-2"> </div>
+            <div class="col-4 theme-frame">
               <router-link :to="{ path: '/tabletMedia', query: { theme: 'red' }}" id="tabletMedia">
                 <a href="#" class>
                   <div class="col 6 theme-boxes red-theme">
-                    <div class="row theme-position theme-position-red">
-                      <img alt src="../assets/Familie-Icon.png" />
-                      <h1 class="theme-text">
-                        
-                      </h1>
+                    <div class="row theme-position theme-position-red imgCenter">
+                      <img  alt src="../assets/Familie-Icon.png" />
+                      <h1 class="theme-text"></h1>
                     </div>
                   </div>
                 </a>
               </router-link>
             </div>
-            <div class="col 6 theme-frame">
+            <div class="col-4 theme-frame">
               <router-link :to="{ path: '/tabletMedia', query: { theme: 'blue' }}" id="tabletMedia">
                 <a href="#" class>
                   <div class="col 6 theme-boxes blue-theme">
-                    <div class="row theme-position">
+                    <div class="row theme-position imgCenter">
                       <img alt src="../assets/Heimat-Icon.png" />
-                      <h1 class="theme-text">
-                        
-                      </h1>
+                      <h1 class="theme-text"></h1>
                     </div>
                   </div>
                 </a>
               </router-link>
             </div>
           </div>
-          <div class="row">
-            <div class="col 2 theme-frame">
+          <br>
+          <div class="row" align="center">
+            <!-- Zur Zentrieerung -->
+            <div class="col-2"> </div>
+            <div class="col-4 theme-frame">
               <router-link
                 :to="{ path: '/tabletMedia', query: { theme: 'green' }}"
                 id="tabletMedia"
               >
                 <a href="#" class>
                   <div class="col 6 theme-boxes green-theme">
-                    <div class="row theme-position theme-position-green">
+                    <div class="row theme-position theme-position-green imgCenter">
                       <img alt src="../assets/Natur-Icon.png" />
-                      <h1 class="theme-text">
-                        
-                      </h1>
+                      <h1 class="theme-text"></h1>
                     </div>
                   </div>
                 </a>
               </router-link>
             </div>
-            <div class="col 6 theme-frame">
+            <div class="col-4 theme-frame">
               <router-link
                 :to="{ path: '/tabletMedia', query: { theme: 'orange' }}"
                 id="tabletMedia"
               >
                 <a href="#" class>
                   <div class="col 6 theme-boxes orange-theme">
-                    <div class="row theme-position">
+                    <div class="row theme-position imgCenter">
                       <img alt src="../assets/Freizeit-Icon.png" />
-                      <h1 class="theme-text">
-                        
-                      </h1>
+                      <h1 class="theme-text"></h1>
                     </div>
                   </div>
                 </a>
@@ -141,98 +149,47 @@ export default {
       newUser: { name: "", password: "" }
     };
   },
-  methods: {
-    // add new User to databank with a username
-    addNewUser() {
-      axios
-        .post("http://139.6.102.67:8080/users", this.newUser)
-        .then(res => {
-          if (res.status == 200) {
-            console.log(res);
-            this.user = this.user.concat(this.newUser.name);
-            // M.toast({
-            //   html: "<b>Added new user @" + this.newUser.name + "</b>",
-            //   classes: "green white-text"
-            // });
-            this.toggleElements("createUser");
-          }
-        })
-        .catch(err => {
-          if (err.response.status == 404 || err.response.status == 401) {
-            M.toast({
-              html: "<b>Could not add new user @" + this.newUser.name + "</b>",
-              classes: "red white-text"
-            });
-          }
-        });
-    },
-    // Delete User with given ID
-    deleteUser(id) {
-      console.log("delete User with ID: " + id);
-      axios
-        .delete("http://139.6.102.67:8080/users/" + id)
-        .then(res => {
-          if (res.status == 200) {
-            console.log(res);
-            this.user = this.user.filter(user => user !== id);
-            //  M.toast({
-            //   html: "<b>Deleted user @" + id + "</b>",
-            //   classes: "green white-text"
-            //  });
-            //  var elem = document.getElementById(id);
-            //  var instance = M.Modal.getInstance(elem);
-            //  instance.close();
-          }
-        })
-        .catch(err => {
-          if (err.response.status == 404 || err.response.status == 401) {
-            //  M.toast({
-            //    html: "<b>Could not delete user @" + id + "</b>",
-            //    classes: "red white-text"
-            //  });
-            console.log("Delete User with Error: " + err);
-          }
-        });
-    }
-  },
-  created() {
-    axios
-      .get("http://139.6.102.67:8080/users")
-      .then(res => {
-        console.log(res.data);
-        this.user = res.data;
-      })
-      .catch(err => console.log("Hey! Axios error for Users: " + err));
-  }
+  methods: {},
+  created() {}
 };
 </script>
 
 <style scoped>
-h2{
-  margin-top: 30px;
+h2 {
+  font-size: 350%;
+  margin-top: 100px;
+  margin-bottom: 50px;
+}
+h3 {
+  font-weight: bold;
+  margin-top: 80px;
+  margin-bottom: 20px;
 }
 .theme-boxes {
-  height: 30vh;
+  height: 20vh;
+  width: 20vh;
   padding: 0;
-  border-radius: 50px;
+  /* border-radius: 50px; */
   box-shadow: 0px 5px 5px #303030;
 }
-.theme-frame {
-  margin-top: 20px;
+img {
+  height: 160px;
+  width: 160px;
 }
-img{
-  height: 150px;
-  width: auto;
+.imgCenter {
+  padding-top: 20px;
+  padding-left: 30px;
+}
+.rounded {
+  height: 200px;
+  width: 200px;
 }
 .UserSpalte {
   max-height: 100px;
 }
-h3 {
-  font-weight: bold;
-}
 .headerDivider {
   border-left: 4px solid grey;
-  height: 500px;
+  height: 700px;
   position: absolute;
   left: 50%;
   margin-left: -2px;
@@ -241,11 +198,5 @@ h3 {
 }
 .links {
   line-height: 200px;
-}
-.theme-boxes {
-  height: 30vh;
-  padding: 0;
-  border-radius: 50px;
-  box-shadow: 0px 5px 5px #303030;
 }
 </style>
