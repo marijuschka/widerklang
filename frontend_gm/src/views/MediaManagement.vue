5756f6a24becf7e7dc2462eecd754916b6aaa5dd
Marius <mgoralczyk@aikq.com>
2019-09-19 11:56:58 +0200

    bugfixing2

--- frontend_gm/src/views/MediaManagement.vue
+++ frontend_gm/src/views/MediaManagement.vue
@@ -50,7 +50,8 @@
          <!-- <v-flex> <Popup /></v-flex>
           <Popup /> -->
           
-      </div>
+   
+      
       <draggable
         class="dragArea list-group"
         :list="images"
@@ -70,9 +71,14 @@
           />
         </div>
       </draggable>
+      </div>
         
          <div class="col-3">
-      <h3>Fernseher</h3>
+     
+      </div>
+    </div>
+      <div class="col-3 dragArea">
+       <h3>Fernseher</h3>
       <draggable
         class="dragArea list-group"
         :list="tv"
@@ -88,9 +94,6 @@
             />
           </div>
         </draggable>
-      </div>
-    </div>
-              <div class="col-3">
       <h2>Bilderwand</h2>
       <h3>Bild1</h3>
       <draggable
@@ -384,14 +387,17 @@ body {
 .dragArea list-group {
   width: 200px;
   height: 300px;
+  border-width: 1em;
+  border-color:black;
   background: brown;
   background-color: brown;
-  max-width: 20ßpx;
+  max-width: 20px;
   max-height: 200px;
 }
 
 .list-group-item {
-  background-color: blue;
+border: 0;
+float: left;
 }
 
 .dragArea list-group {
@@ -404,72 +410,4 @@ body {
 }
 
 
-
-
-
-
-
-.modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, .5);
-  display: table;
-  transition: opacity .3s ease;
-}
-
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
-}
-
-.modal-container {
-  width: 300px;
-  margin: 0px auto;
-  padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-  transition: all .3s ease;
-  font-family: Helvetica, Arial, sans-serif;
-}
-
-.modal-header h3 {
-  margin-top: 0;
-  color: #42b983;
-}
-
-.modal-body {
-  margin: 20px 0;
-}
-
-.modal-default-button {
-  float: right;
-}
-
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
-
-.modal-enter {
-  opacity: 0;
-}
-
-.modal-leave-active {
-  opacity: 0;
-}
-
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
 </style> 
